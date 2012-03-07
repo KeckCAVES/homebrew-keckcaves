6d26eaaa7ba91011e080d788fbb0001a416d127e
Braden Pellett <bpellett@ucdavis.edu>
2012-03-06 23:24:09 -0800

    Fixed MD5 sum for vrui-examples

--- vrui-examples.rb
+++ vrui-examples.rb
@@ -3,7 +3,7 @@ require 'formula'
 class VruiExamples < Formula
   url 'http://keckcaves.org/releases/src/Vrui-2.2-003-1.tar.bz2'
   homepage 'http://keckcaves.org/software/vrui'
-  md5 'e894cfa8e021d130fc0f71802cb35635'
+  md5 'e1c03da47cc1ab7fff7dbdd7dce0968c'
   version "2.2-003-1"
 
 #  depends_on 'vrui'
