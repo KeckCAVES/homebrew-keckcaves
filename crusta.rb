7bff65ecd6a77047626d18ad17e50f240c501ebd
Braden Pellett <bpellett@ucdavis.edu>
2012-03-08 15:14:20 -0800

    The version of Crusta never really increased, and it doesn't help with reinstalling anyway.

--- crusta.rb
+++ crusta.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Crusta < Formula
   homepage 'http://keckcaves.org/software/crusta'
   url 'https://github.com/KeckCAVES/crusta.git', :tag => '89a80e5ebfa3ab8224bbded84043b794c4a1c0cf'
-  version "1.0.3"
+  version "1.0.0"
 
   depends_on 'cmake' => :build
 #  depends_on 'vrui'
