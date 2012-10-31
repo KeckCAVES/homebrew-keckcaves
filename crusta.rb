c5f0ebca2a27c48fce27959a3bff45668e53f4e3
Braden Pellett <bpellett@ucdavis.edu>
2012-10-30 23:11:36 -0700

    Update to more recent crusta.  Provide a url for the --HEAD option.  Use Homebrew's "option" syntax.

--- crusta.rb
+++ crusta.rb
@@ -2,30 +2,22 @@ require 'formula'
 
 class Crusta < Formula
   homepage 'http://keckcaves.org/software/crusta'
-  if ARGV.include? '--enable-slicing'
-    url 'https://github.com/KeckCAVES/crusta.git', :branch => 'slicing', :revision => '34b2a6f2a5490ed097a593cf72b29bd83a3ffe83'
-    ohai 'Enabled slicing tool.'
-  else
-    url 'https://github.com/KeckCAVES/crusta.git', :revision => '522d6b92366040ec1e27725bc844db3e2ead31b0'
-  end
-  version "1.0.0-1"
+  url 'https://github.com/KeckCAVES/crusta.git', :revision => '49a5ae6049178603b4fe1f0f51cb5fcbbfb79fd2'
+  head 'https://github.com/KeckCAVES/crusta.git'
+  version "1.0.0-2"
 
   depends_on 'cmake' => :build
   depends_on 'KeckCAVES/vrui/vrui'
   depends_on 'gdal'
 
-  def options
-    [['--enable-slicing',
-      "Enable the slicing tool, which may slow down other things."]]
-  end
+  option 'enable-slicing', 'Enable the slicing tool, which may slow down or break other things'
 
   def install
     args = []
-    args << "-DCMAKE_BUILD_TYPE=Release"
     args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
     args << "-DCMAKE_PREFIX_PATH=#{HOMEBREW_PREFIX}"
-    args << "-DVRUI_MAKEINCLUDE=#{HOMEBREW_PREFIX}/share/Vrui-2.4/Vrui.makeinclude"
-    system "cmake", "build", *args
+    args << "-DCRUSTA_SLICING=ON" if build.include? 'enable-slicing'
+    system "cmake", ".", *args
     system "make"
     system "make", "install"
   end
