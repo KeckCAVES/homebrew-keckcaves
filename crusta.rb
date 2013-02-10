6f91751aa7fba77bb6f3406f199c268d56028ad5
Braden <contact@dabrado.net>
2013-02-10 09:57:14 -0800

    Install crusta support files under properly named subdirectories.

--- crusta.rb
+++ crusta.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Crusta < Formula
   ver = "1.0-76-g5be5af9"
-  pkgver = "1"
+  pkgver = "2"
 
   homepage "http://keckcaves.org/software/crusta"
   url "https://github.com/KeckCAVES/crusta.git", :revision => "v#{ver}"
@@ -17,11 +17,11 @@ class Crusta < Formula
   def install
     args = []
     args << "-DPROJECT_VERSION=#{version}"
+    args << "-DINSTALL_TYPE=Local"
     args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
     args << "-DETC_DIR=#{etc}/#{name}"
     args << "-DCMAKE_PREFIX_PATH=#{HOMEBREW_PREFIX}"
-    args << "-DCMAKE_RULE_MESSAGES=OFF"
-    args << "-DCMAKE_VERBOSE_MAKEFILE=ON"
+    args << "-DCMAKE_RULE_MESSAGES=OFF" << "-DCMAKE_VERBOSE_MAKEFILE=ON"
     system "cmake", ".", *args
     system "make", "install"
   end
