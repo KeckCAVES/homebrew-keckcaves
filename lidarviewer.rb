4b8c8eb91d18075b7542497674119370bfdc1e91
Gabriele Facciolo <gfacciol@gmail.com>
2016-07-28 00:46:59 +0200

    Fix for issues #3 & #4, compiling in Yosemite & ElCapitan

--- lidarviewer.rb
+++ lidarviewer.rb
@@ -10,6 +10,25 @@ class Lidarviewer < Formula
 
   depends_on 'KeckCAVES/keckcaves/vrui'
 
+  # Patch for Yosemite & ElCapitan to fix the error
+  # "error: cast from pointer to smaller type 'GLuint' (aka 'unsigned int') loses information"
+  patch :p0, "--- PointBasedLightingShader.cpp	2016-07-28 00:13:57.000000000 +0200
++++ PointBasedLightingShader.cpp	2016-07-27 23:42:18.000000000 +0200
+@@ -252,9 +252,9 @@
+ 		glCompileShaderFromString(geometryShader,geometryShaderSource);
+
+ 		/* Set the geometry shader's parameters: */
+-		glProgramParameteriARB(programObject,GL_GEOMETRY_VERTICES_OUT_ARB,4);
+-		glProgramParameteriARB(programObject,GL_GEOMETRY_INPUT_TYPE_ARB,GL_POINTS);
+-		glProgramParameteriARB(programObject,GL_GEOMETRY_OUTPUT_TYPE_ARB,GL_TRIANGLE_STRIP);
++		glProgramParameteriARB(reinterpret_cast<uintptr_t>(programObject),GL_GEOMETRY_VERTICES_OUT_ARB,4);
++		glProgramParameteriARB(reinterpret_cast<uintptr_t>(programObject),GL_GEOMETRY_INPUT_TYPE_ARB,GL_POINTS);
++		glProgramParameteriARB(reinterpret_cast<uintptr_t>(programObject),GL_GEOMETRY_OUTPUT_TYPE_ARB,GL_TRIANGLE_STRIP);
+
+ 		/* Compile the surfel fragment shader: */
+ 		const char* fragmentShaderSource=
+"
+
   def install
     args = []
     args << "INSTALLDIR=#{prefix}"
