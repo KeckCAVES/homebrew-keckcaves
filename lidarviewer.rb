9588663558354e7e2769a74e41daec586243e8e1
Gabriele Facciolo <gfacciol@gmail.com>
2016-10-02 01:32:00 +0200

    make the forked repository functional

--- lidarviewer.rb
+++ lidarviewer.rb
@@ -8,7 +8,7 @@ class Lidarviewer < Formula
   url "https://github.com/KeckCAVES/LidarViewer.git", :revision => "v#{ver}"
   version "#{ver}-#{pkgver}"
 
-  depends_on 'KeckCAVES/keckcaves/vrui'
+  depends_on 'gfacciol/keckcaves/vrui'
 
   # Patch for Yosemite & ElCapitan to fix the error
   # "error: cast from pointer to smaller type 'GLuint' (aka 'unsigned int') loses information"
